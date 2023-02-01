f7759464d5a9e68bd7ba9bc0b73c74f620a85ef4
timur <timurkarpinskii02@gmail.com>
2023-02-01 16:58:14 +0300

    Гатлим

--- src/test/java/LoadTest.scala
+++ src/test/java/LoadTest.scala
@@ -5,12 +5,16 @@ import io.gatling.core.Predef._
 import scala.concurrent.duration._
 import scala.language.postfixOps
 class LoadTest extends Simulation{
-  val httpConf=http.baseUrl("https://demoqa.com/swagger/")
+  val httpConf=http.baseUrl("https://demoqa.com/swagger/#/")
 
-  val snc = scenario("First") randomSwitch(
-    (100,account)
+  val snc = scenario("First")randomSwitch(
+    (25,account),
+    (25,getUser),
+    (25,deleteUser),
+    (25,postUser)
   )
 
-  setUp(snc.inject(constantUsersPerSec(100)during(20 seconds))
-    .protocols(httpConf))
+  setUp(
+    snc.inject(constantUsersPerSec(100)during(25 minute))
+      .protocols(httpConf))
 }
--- src/test/java/Request.scala
+++ src/test/java/Request.scala
@@ -1,10 +1,37 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import java.util.UUID
+
 object Request {
-  val account =exec(
-    http("account")
-    .get("/Account/v1/User")
-    .check(status.is(200))
+  val account =
+    exec(
+      http("post")
+        .post("/Account/v1/Authorized")
+        .check(status.is(200))
+    )
+  val getUser = exec(
+    session => session.set("UUID", UUID.randomUUID().toString)
+  ).exec(
+    http("get")
+      .get("/Account/v1/User/${UUID}")
+      .check(status.is(200))
+  )
+  val deleteUser = exec(
+    session => session.set("UUID", UUID.randomUUID().toString)
+  ).exec(
+    http("delete")
+      .delete("/Account/v1/User/{UUID}")
+      .check(status.is(200))
+  )
+  val postUser = exec(
+    session => session.set("userName", UUID.randomUUID().toString)
+  ).exec(
+    session => session.set("password", UUID.randomUUID().toString)
+  ).exec(
+    http( "new user")
+      .post("/Account/v1/User")
+      .body(ElFileBody("body.json")).asJson
+      .check(status.is(200))
   )
 }
